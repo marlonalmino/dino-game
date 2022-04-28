c522dff6758938e1843065e32a38a76809289b1d
Marlon Almino <alminomarlon@gmail.com>
2022-04-28 15:31:08 -0300

    cactus movement and loop

--- script.js
+++ script.js
@@ -39,10 +39,23 @@ function jump() {
 function createCactus() {
   const cactus = document.createElement('div')
   let cactusPosition = 1000
+  let randomTime = Math.random() * 6000
 
   cactus.classList.add('cactus')
   cactus.style.left = 1000 + 'px'
   background.appendChild(cactus)
+
+  let leftInterval = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval)
+      background.removeChild(cactus)
+    } else {
+      cactusPosition -= 10
+      cactus.style.left = `${cactusPosition}px`
+    }
+  }, 20)
+
+  setTimeout(createCactus, randomTime)
 }
 
 createCactus()

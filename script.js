fca50bfba195d7daeed7bc28528e598bc1b2edad
Marlon Almino <alminomarlon@gmail.com>
2022-04-28 15:13:06 -0300

    implemented function to make dino jumps

--- script.js
+++ script.js
@@ -2,8 +2,32 @@ const dino = document.querySelector('.dino')
 
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
-    console.log('Pressionou Espaço!')
+    jump()
   }
+}
+
+function jump() {
+  let position = 0
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      clearInterval(upInterval)
+
+      // Descendo
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval)
+        } else {
+          position -= 20
+          dino.style.bottom = `${position}px`
+        }
+      }, 20)
+    } else {
+      // Subindo
+      position += 20
+      dino.style.bottom = `${position}px`
+    }
+  }, 20)
 }
 
 document.addEventListener('keyup', handleKeyUp)
